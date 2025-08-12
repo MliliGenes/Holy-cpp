816f19f6f22926e5fd4b7d0b9ce96581c5e5f0cd
MliliGenes <saad.elm.77@gmail.com>
2025-08-12 23:44:35 +0100

    be more positive abiut this crap

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+// #include ""
+// #include ""
+
+
+class Fixed {
+
+    private :
+
+        int fixedPoint;
+        const static int fractionalBits = 8;
+
+    public :
+
+        Fixed( void );
+        Fixed( const Fixed& other );
+        Fixed& operator=( const Fixed& other);
+        ~Fixed( void );
+
+        int getRawBits( void ) const;
+        void setRawBits( const int raw );
+
+        Fixed( const int rawInt );
+        Fixed( const float rawFloat );
+
+        float toFloat( void ) const;
+        int toInt( void ) const;
+
+}
