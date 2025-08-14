53aba4508298adc8a1bc93f6b9700d5f5f4c545a
MliliGenes <saad.elm.77@gmail.com>
2025-08-14 15:55:55 +0100

    Fixed and Float are overrated, use binary

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,81 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ) : fixedPoint(0) {
+
+    std::cout << "Default constructor called" << std::endl;
+
+}
+
+Fixed::Fixed( const Fixed& other ) {
+
+    std::cout << "Copy constructor called" << std::endl;
+    fixedPoint = other.getRawBits();
+
+}
+
+Fixed& Fixed::operator=( const Fixed& other ) {
+
+    std::cout << "Copy assignment operator called" << std::endl;
+
+    if ( this  == &other )
+        return *this;
+
+    fixedPoint = other.getRawBits();
+
+    return *this;
+
+}
+
+Fixed::~Fixed( void ) {
+
+    std::cout << "Destructor called" << std::endl;
+
+}
+
+
+void Fixed::setRawBits( const int raw ) {
+
+    std::cout << "meoooooow" << std::endl;
+    fixedPoint = raw;
+
+}
+
+
+int Fixed::getRawBits( void ) const {
+
+    std::cout << "getRawBits member function called" << std::endl;
+    return fixedPoint;
+
+}
+
+Fixed::Fixed( const int rawInt ) {
+
+    std::cout << "Int constructor called" << std::endl;
+    fixedPoint = rawInt << fractionalBits;
+
+}
+
+int Fixed::toInt( void ) const {
+
+    return fixedPoint >> fractionalBits;
+
+}
+
+Fixed::Fixed( const float rawFloat ) {
+
+    std::cout << "Float constructor called" << std::endl;
+    fixedPoint = roundf( rawFloat * ( 1 << fractionalBits ) );
+
+}
+
+float Fixed::toFloat( void ) const {
+
+    return (float)fixedPoint / ( 1 << fractionalBits );
+
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+    os << fixed.toFloat(); // insert the float version into the stream
+    return os; // allow chaining: cout << a << b;
+}
+
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+// #include ""
+
+
+class Fixed {
+
+    private :
+
+        int fixedPoint;
+        const static int fractionalBits = 8;
+
+    public :
+
+        Fixed( void );
+        Fixed( const Fixed& other );
+        Fixed& operator=( const Fixed& other);
+        ~Fixed( void );
+
+        int getRawBits( void ) const;
+        void setRawBits( const int raw );
+
+        Fixed( const int rawInt );
+        Fixed( const float rawFloat );
+
+        float toFloat( void ) const;
+        int toInt( void ) const;
+
+};
+
+std::ostream& operator<<( std::ostream&, const Fixed& );
