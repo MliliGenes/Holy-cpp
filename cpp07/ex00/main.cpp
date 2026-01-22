00145ee47372129b2efa261073d4977005c9f568
MliliGenes <saad.elm.77@gmail.com>
2026-01-22 04:41:19 +0100

    i still think about you, wass i wish it didn't end that way

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,25 @@
+#include "whatever.hpp"
+#include <iostream>
+
+int main() {
+    float a = 2;
+    float b = 3;
+
+    std::cout << "[default] " << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "[swapped] " << "a = " << a << ", b = " << b << std::endl;
+
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "123";
+    std::string d = "1234";
+
+    std::cout << "[default] " << "c = " << c << ", d = " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "[swapped] " << "c = " << c << ", d = " << d << std::endl;
+
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+}
